ea7d145b0a5152b7372a75b925674f34b6fd725a
CurtisCutter <ccutt826@mtroyal.ca>
2026-04-07 15:41:53 +0000

    Not quite done yet.

--- src/main/LootManager.java
+++ src/main/LootManager.java
@@ -2,6 +2,9 @@ package main;
 
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
 
 /**
  * Manages the inventory of RPG Loot.
@@ -25,5 +28,45 @@ public class LootManager {
         }
         System.out.println("-------------------------");
         System.out.println();
+    }
+
+    public static LootManager load(String filePath) throws FileNotFoundException {
+
+        LootManager lootManager = new LootManager();
+
+        Scanner fileScanner = new Scanner(new File(filePath));
+
+        fileScanner.nextLine();
+
+        while (fileScanner.hasNextLine()) {
+            String[] entityParts = fileScanner.nextLine().split(",");
+
+            Loot entity = LootFactory.create(entityParts);
+            lootManager.add(entity);
+        }
+
+        fileScanner.close();
+
+        return lootManager;
+    }
+
+    public void add(Loot entity) {
+        if (entity != null) {
+            inventory.add(entity);
+        }
+    }
+
+    public void save(String filePath) throws FileNotFoundException {
+
+        PrintWriter fileWriter = new PrintWriter(new File(filePath));
+
+        // 2. Remember to write the header.
+        fileWriter.println("TYPE,NAME,RARITY,SPECIAL_1");
+
+        for (Loot currEntity : inventory) {
+            fileWriter.println(currEntity.asCsvRecord());
+        }
+        fileWriter.close();
+
     }
 }
---
+++ src/main/Weapon.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Weapon extends Loot {
+    
+    private int damage;
+
+    public Weapon(String name, String rarity, int damage) {
+        super(name, rarity);
+        this.damage = damage;
+    }
+
+    @Override
+    public String getEffectDescription() {
+        return String.format("%s (%s): %d atk", getName(), getRarity(), this.damage);
+    }
+
+    @Override
+    public String asCsvRecord() {
+        return String.format("Weapon,%s,%s,%d", getName(), getRarity(), this.damage);
+    }
+}
+
